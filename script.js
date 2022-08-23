6c4a77e0f8c65ba00fa5022147dde0597cbaf9d6
Erick Kuwahara <80263417+ErickKS@users.noreply.github.com>
2022-08-23 17:28:05 -0300

    Calculator v2.0

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 const resultCalc = document.getElementById('result')
 
+const popUp = document.getElementById('pop-upLayer');
+
 function insert(int) {
-    let number = resultCalc.innerHTML
-    resultCalc.innerHTML = number + int
+    let number = resultCalc.innerHTML;
+    resultCalc.innerHTML = number + int;
+
+    const displayNumbers = resultCalc.innerText;
+    if (displayNumbers.length == 9) {
+        popUp.classList.add('active');
+    }
 }
 
-function ac() {
-    resultCalc.innerHTML = ""
+function closePopUp() {
+    popUp.classList.remove('active');
+}
+
+function c() {
+    resultCalc.innerHTML = "";
 }
 
 function erase() {
-    const newResult = resultCalc.innerHTML
-    resultCalc.innerHTML = newResult.substring(0, newResult.length - 1)
+    const newResult = resultCalc.innerHTML;
+    resultCalc.innerHTML = newResult.substring(0, newResult.length - 1);
 }
 
 function errorAlert() {
     setTimeout(() => {
         const alert = document.getElementById('alert');
-        alert.style.opacity = '1'
+        alert.style.opacity = '1';
         if(alert.style.opacity == '1'){
             setTimeout(() => {
                 alert.style.opacity = '0';
@@ -25,11 +36,11 @@ function errorAlert() {
         }
     })
     clearTimeout();
-    ac();
+    c();
 }
 
 function calc() {
-    const newResult = resultCalc.innerHTML
+    const newResult = resultCalc.innerHTML;
     if(newResult.startsWith('x') || newResult.startsWith('/')) {
         errorAlert()
     }else if(newResult.includes('--') || newResult.includes('-+') || newResult.includes('-.') || newResult.includes('-x') || newResult.includes('-/') 
@@ -47,3 +58,14 @@ function calc() {
         resultCalc.innerHTML = eval(newResult);
     }
 }
+
+// THEME TOGGLER
+
+const themeToggler = document.querySelector(".theme__toggler input");
+const themeTogglerBtn = document.querySelector(".theme__toggler");
+
+themeToggler.addEventListener('click', () => {
+    document.body.classList.toggle('dark__theme__variables');
+
+    themeTogglerBtn.classList.toggle('active');
+});
